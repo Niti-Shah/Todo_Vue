dde2592f97e7271fe95ab4f34d13051d1a98156f
Niti Shah <emailtonitishah@gmail.com>
2021-07-29 19:42:20 +0530

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about pa-10">
+    <h1>About my ToDo App using Vuetify</h1>
+    <br>
+    <br>
+    <p>
+      This is the first time I am trying my hands on Vue and Vuetify!
+      Feels similar to React. 
+      Let's see what's in store next!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+
+  </div>
+</template>
